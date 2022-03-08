e11c56e322296aa4d23cdf13b4842926071e0684
MeiLi <gymmyp1@gmail.com>
2022-03-08 11:36:45 -0500

    added stats to block density

--- block-density.py
+++ block-density.py
@@ -6,27 +6,8 @@ import random
 import math
 import datetime
 import numpy as np
+from collections import Counter
 sys.path.append('../')
-
-'''def read_file():
-    # read the data in. It is assumed that the file contains information in the following format:
-    #   idx1 idx2 ... idxn value
-    # each index is stored as a tuple
-    indexes = []
-    with open(sys.argv[1], 'r') as file:
-        #determine what is the moving index
-        # save the other 3 indexes
-
-
-
-        for row in file:
-            # read in the row and discard its value field
-            row = row.split(' ')
-            row.pop()
-
-            # append the index as a list to the main list
-            indexes.append(tuple(int(i) for i in row))
-            '''
 
 def main():
     #quick and dirty version for now
@@ -52,14 +33,19 @@ def main():
                 print(row[i])
                 row.pop(i)'''
 
-            #nips tensor
+            #nips/nell tensors
+            row.pop(2)
+
+            #enron
             #row.pop(2)
+            #row.pop(3)
 
             #uber
             #row.pop()
             #row.pop()
 
             #lbnl
+            #row.pop(4)
 
             # append the index as a list to the main list
             curr_index = tuple(int(i) for i in row)
@@ -73,14 +59,29 @@ def main():
                 indexes[curr_index] += 1
                 #print('repeat of index: ', curr_index)
 
-        #print(indexes)
         highest_value = 0
+        avg = 0
         for key, value in indexes.items():
             if value > highest_value:
                 highest_value = value
-                #print(key,value)
+            avg += value
 
+        avg = avg/len(indexes)
+
+        #retrieve highest 5 values
+        top5avg = 0
+        top5 = dict(Counter(indexes).most_common(5))
+        print(top5)
+        #for value in top5.items():
+        #    top5avg += value
+
+        #top5avg = avg/5
+
+        print("overall average block density:",avg)
+        print("top5avg: ",top5avg)
         print('highest block density:', highest_value)
+
+        #should plot this...
 
 if __name__ == "__main__":
    main()
